1fa76ae639fec34d39402196bd65b58d88137ad2
unknown <20010109@usc.edu.ph>
2025-03-19 15:22:50 +0800

    ACT 7

--- post-service/index.js
+++ post-service/index.js
@@ -1,7 +1,10 @@
 const { ApolloServer, gql } = require('apollo-server');
 const { PrismaClient } = require('@prisma/client');
+const { PubSub } = require('graphql-subscriptions'); // Import PubSub
+
 const prisma = new PrismaClient();
- 
+const pubsub = new PubSub(); // Create a PubSub instance
+
 // GraphQL Schema
 const typeDefs = gql`
   type Post {
@@ -9,19 +12,25 @@ const typeDefs = gql`
     title: String!
     content: String!
   }
- 
+
   type Query {
     posts: [Post!]!
     post(id: Int!): Post
   }
- 
+
   type Mutation {
     createPost(title: String!, content: String!): Post!
     updatePost(id: Int!, title: String, content: String): Post!
     deletePost(id: Int!): Post!
   }
+
+  type Subscription {
+    postCreated: Post!
+    postUpdated: Post!
+    postDeleted: Post!
+  }
 `;
- 
+
 // Resolvers
 const resolvers = {
   Query: {
@@ -30,22 +39,56 @@ const resolvers = {
   },
   Mutation: {
     createPost: (_, args) => {
-      return prisma.post.create({ data: { title: args.title, content: args.content } });
+      const newPost = prisma.post.create({ data: { title: args.title, content: args.content } });
+
+      // Publish the new post to the subscription
+      pubsub.publish('POST_CREATED', { postCreated: newPost });
+
+      return newPost;
     },
     updatePost: (_, args) => {
-      return prisma.post.update({
+      const updatedPost = prisma.post.update({
         where: { id: args.id },
         data: { title: args.title, content: args.content },
       });
+
+      // Publish the updated post to the subscription
+      pubsub.publish('POST_UPDATED', { postUpdated: updatedPost });
+
+      return updatedPost;
     },
     deletePost: (_, args) => {
-      return prisma.post.delete({ where: { id: args.id } });
+      const deletedPost = prisma.post.delete({ where: { id: args.id } });
+
+      // Publish the deleted post to the subscription
+      pubsub.publish('POST_DELETED', { postDeleted: deletedPost });
+
+      return deletedPost;
+    },
+  },
+  Subscription: {
+    postCreated: {
+      subscribe: () => pubsub.asyncIterator(['POST_CREATED']), // Listen for post creation events
+    },
+    postUpdated: {
+      subscribe: () => pubsub.asyncIterator(['POST_UPDATED']), // Listen for post update events
+    },
+    postDeleted: {
+      subscribe: () => pubsub.asyncIterator(['POST_DELETED']), // Listen for post deletion events
     },
   },
 };
- 
+
 // Start the Apollo Server
-const server = new ApolloServer({ typeDefs, resolvers });
-server.listen({ port: 4002 }).then(({ url }) => {
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  subscriptions: {
+    path: '/subscriptions', // Subscription endpoint
+  },
+});
+
+server.listen({ port: 4002 }).then(({ url, subscriptionsUrl }) => {
   console.log(`Posts service running at ${url}`);
+  console.log(`Subscriptions ready at ${subscriptionsUrl}`);
 });
